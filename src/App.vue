e46aed785bf7783f86767db34d1e84290690498c
EvgeniiMorozov <emorozov1101@gmail.com>
2021-09-07 21:16:06 +0500

    урок 8 - end

--- src/App.vue
+++ src/App.vue
@@ -1,18 +1,39 @@
 <template>
   <div id="app">
-    <button @click="onClick">Click event</button>
+    <button @click.right="onClick('some value', $event)">Click event</button>
+    <a href @click.prevent="onLinkClick">Link</a>
+    <h1>{{ text }}</h1>
+    <input type="text" @keyup.enter="onKeyUp" />
   </div>
 </template>
 
 <script>
 export default {
-  data: () => ({}),
+  data: () => ({
+    text: '',
+  }),
   methods: {
-    onClick(e) {
-      console.log('click', e);
+    onClick(value, e) {
+      console.log(value, e);
+    },
+    onLinkClick() {
+      console.log('link click');
+    },
+    onKeyUp(e) {
+      console.log(e);
+      this.text = e.target.value;
     },
   },
 };
 </script>
 
-<style scoped></style>
+<style lang="scss">
+#app {
+  font-family: Avenir, Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 60px;
+}
+</style>
