c1292216378eeabfcbd4fc4d1265824e5e845920
Evgenii Morozov <zheka1101@yandex.ru>
2021-09-07 16:34:22 +0500

    урок 7

--- src/App.vue
+++ src/App.vue
@@ -5,6 +5,29 @@
     <h3>{{ num > 1 ? 'Number > 1' : 'Number lower 1' }}</h3>
     <a :href="linkUrl">Link</a>
     <a :[attrKey]="linkUrl">Link</a>
+    <template v-if="msgIsVisible === 1">
+      <div class="message">Some Message!</div>
+    </template>
+    <template v-else-if="msgIsVisible === 2">
+      <div class="message">V-If-Else Some Message!</div>
+    </template>
+    <template v-else>
+      <div class="message">Else Message!</div>
+    </template>
+
+    <div v-show="isVisibleBlock">
+      V Show Example
+    </div>
+
+    <div class="name-input" v-if="inputNameVisible">
+      <label>Name</label>
+      <input type="text" placeholder="input name" key="name-input">
+    </div>
+    <div class="nickname-input" v-else>
+      <label>Nickname</label>
+      <input type="text" placeholder="input nickname" key="nickname-input">
+    </div>
+    <button @click="inputNameVisible = !inputNameVisible">Change form</button>
   </div>
 </template>
 
@@ -20,6 +43,9 @@ export default {
     num: 2,
     linkUrl: 'https://google.ru',
     attrKey: 'href',
+    msgIsVisible: 1,
+    isVisibleBlock: true,
+    inputNameVisible: true,
   }),
 };
 </script>
