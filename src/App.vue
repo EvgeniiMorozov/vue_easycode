7eec645571c957c4603b4569bb0c8807d554b05f
Evgenii Morozov <zheka1101@yandex.ru>
2021-09-07 17:03:48 +0500

    урок 8 - 02-49

--- src/App.vue
+++ src/App.vue
@@ -1,10 +1,17 @@
 <template>
-  <div id="app"></div>
+  <div id="app">
+    <button @click="onClick">Click event</button>
+  </div>
 </template>
 
 <script>
 export default {
   data: () => ({}),
+  methods: {
+    onClick(e) {
+      console.log('click', e);
+    },
+  },
 };
 </script>
 
